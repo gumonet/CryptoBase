869b44c21c52f2cb94fc90b633cf5bd13b7f0427
Angel Gutierrez Monola <gutierrez.monola@gmail.com>
2021-12-13 18:49:45 -0600

    Finish v1

--- popup.js
+++ popup.js
@@ -1,7 +1,74 @@
-const baseCoinAPI = "https://pro-api.coinmarketcap.com/";
+
+/*function getQuote(){
+  fetch("https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd")
+  .then( ( response ) => {
+    if( response.ok ) {
+      return response.json();
+    } 
+    return response.text().then( ( error ) => { console.log(error); throw error } );
+  })
+  .then (( response ) =>  {
+     //quote.textContent = ( typeof response[0] !== 'unfdefined' ) ?  response[0] :  "Doesn't exist quote to show" ;
+     console.log( response );
+  })
+  .catch ( ( error ) => { 
+      console.warn( error );
+  })
+}*/
+
+async function getDataApi() {
+  let response = await fetch("https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd");
+
+  try{
+    if ( ! response.ok ){
+      throw response.statusText;
+      return false;
+    }
+
+    let cripto_data = await response.json();
+    return cripto_data;
+
+  } catch( error ){
+      console.warn( error );
+      return false;
+  }
+  
+}
+
+
+async function init() {
+
+  let cripto_data = await getDataApi();
+
+  if( cripto_data !== false ) {
+    
+    if( cripto_data.data.length >  0 ){
+      const mainContent = document.getElementById("main-content");
+      cripto_data.data.forEach((coin) => {
+        let content = document.createElement("div");
+        content.classList.add('item-cripto');
+        //let icon = document.createElement("img");
+        let name_content = document.createElement("span");
+        let price_content = document.createElement("span");
+        name_content.classList.add('cripto-price');
+        
+        name_content.textContent = `${coin.symbol}`;
+        price_content.textContent = `$ ${coin.metrics.market_data.price_usd} usd`;
+        content.appendChild(name_content);
+        content.appendChild(price_content);
+        mainContent.appendChild(content);
+      });
+    }
+
+  }
+
+}
+
+init();
+
+/*const baseCoinAPI = "https://pro-api.coinmarketcap.com/";
 const apiVersion = "v1";
 const apiKey = `&CMC_PRO_API_KEY=ac654499-0e7f-4dc6-8945-809d7707a78e`;
-
 //Function to get the top 25 coins.
 const getCoins = async (endPoint) => {
   let path = `${baseCoinAPI}${apiVersion}${endPoint}${apiKey}`;
@@ -34,4 +101,4 @@ if (coins.length > 0) {
     content.appendChild(paragraph);
     mainContent.appendChild(content);
   });
-}
+}*/
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 ## Resource
 -> Post https://techblog.geekyants.com/building-a-chrome-extension-in-2021
+https://developer.chrome.com/docs/extensions/mv3/getstarted/
+https://support.google.com/chrome/a/answer/2714278?hl=es
+
+Los workers es el código que se mantendrá ejecutando mientras la extensión esté activa.
+
+Mediante insert scripts puedes insertar código en páginas especificas.
